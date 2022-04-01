088e53922d0b076c551b3b6b3cbe9f46e57d33ad
João Alberto <hey@joaoalberto.dev>
2022-03-31 23:35:17 -0300

    chore: handling user search and listening to search click

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,30 @@
-import { Center, SearchInput, Title } from "~/ui/components";
+import { useRouter } from "next/router";
+import { Button, Center, SearchInput, Title } from "~/ui/components";
+import { useSearch } from "~/hooks";
+import { FormEvent } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const { query, setQuery } = useSearch();
+
+  function navigateToSearchPage() {
+    router.push(`/search/${query}`);
+  }
+
+  function handleSearchInputChange(event: FormEvent<HTMLInputElement>) {
+    setQuery(event.currentTarget.value);
+  }
+
   return (
     <Center height="100vh" width="100%">
       <Center direction="column" width="400px">
         <Title>Nasa search</Title>
-        <SearchInput />
+        <Center width="100%">
+          <SearchInput onChange={handleSearchInputChange} />
+          <Button onClick={navigateToSearchPage} disabled={!query}>
+            Search
+          </Button>
+        </Center>
       </Center>
     </Center>
   );
